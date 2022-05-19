ecd86cd0e7f6ebe223134920bf7ddfd32b635822
Eduard <ladiesman5511@mail.ru>
2022-05-19 19:38:42 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+import Cards from "./components/cards/Cards";
+import Filters from "./components/filters/Filters";
 
 function App() {
+
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchData] = useState([]);
+  let {info, results} = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then(res=>res.json());
+      updateFetchData(data);
+    })()
+
+  }, [api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center ubuntu my-4">
+        Rick & Morty <span className="text-primary">Wiki</span>
+      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards results={results}/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/cards/Cards.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Cards = ({ results }) => {
+  let display;
+  console.log(results)
+  if (results) {
+    display = results.map(x => {
+      let { id, name, image, location, status } = x
+      return (
+        <div key={id} className='col-4 position-relative'>
+          <div className=''>
+            <img src={image} alt='' className='img-fluid' />
+            <div className='content'>
+              <div className='fs-4 fw-bold mb-4'>{name}</div>
+              <div className=''>
+                <div className='fs-6'>Last location: </div>
+                <div className='fs-5'>{location.name}</div>
+              </div>
+            </div>
+          </div>
+          <div className='position-absolute badge bg-danger'>{status}</div>
+        </div>
+      );
+    });
+  } else {
+    display = "Not Found :(";
+  }
+
+  return (
+    <>{display}</>
+  )
+};
+
+export default Cards;
