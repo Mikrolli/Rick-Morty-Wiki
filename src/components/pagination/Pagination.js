1f638bcac6905220967e862e5087a0dae2fd0de5
Eduard <ladiesman5511@mail.ru>
2022-05-24 14:32:07 +0300

    f

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
-import styles from './Pagination.module.scss';
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
 
